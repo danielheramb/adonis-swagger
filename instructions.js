e6f63357f83e2b053daea52ebb714ab2bb3ab8d0
Daniel Heramb <daniel.heramb@lexisnexisrisk.com>
2020-04-08 16:13:21 -0400

    v2.0.0 : Updating swagger-jsdoc to 4.0.0, removing templates directory, requiring swagger-ui-dist, updating openapi spec to 3.0

--- instructions.js
+++ instructions.js
@@ -1,13 +1,14 @@
 'use strict'
 
 const path = require('path')
+const swaggerUiAssetPath = require("swagger-ui-dist").getAbsoluteFSPath()
 
 module.exports = async function (cli) {
   try {
-    await cli.makeConfig('swagger.js', path.join(__dirname, './templates/config.js'))
+    await cli.makeConfig('swagger.js', path.join(__dirname, './config/swagger.js'))
     cli.command.completed('create', 'config/swagger.js')
 
-    await cli.copy(path.join(__dirname, './templates/docs'), 'public/docs')
+    await cli.copy(path.join(__dirname, swaggerUiAssetPath), 'public/docs')
     cli.command.completed('copy assets', 'public/docs')
   } catch (error) {
     // ignore errors
