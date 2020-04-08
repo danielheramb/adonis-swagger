9ae4ba252e7c2e1b87ceb1f66bafeb7fa77656d2
Daniel Heramb <daniel.heramb@lexisnexisrisk.com>
2020-04-08 16:52:31 -0400

    v2.0.0 : Updating instructions

--- instructions.js
+++ instructions.js
@@ -1,16 +1,18 @@
 'use strict'
 
 const path = require('path')
-const swaggerUiAssetPath = require("swagger-ui-dist").getAbsoluteFSPath()
+const swaggerUiDist = require('swagger-ui-dist')
+const swaggerUiAssetPath = swaggerUiDist.getAbsoluteFSPath()
 
 module.exports = async function (cli) {
   try {
-    await cli.makeConfig('swagger.js', path.join(__dirname, './config/swagger.js'))
+    await cli.makeConfig('swagger.js', path.join(__dirname, 'config/swagger.js'))
     cli.command.completed('create', 'config/swagger.js')
 
     await cli.copy(path.join(__dirname, swaggerUiAssetPath), 'public/docs')
     cli.command.completed('copy assets', 'public/docs')
   } catch (error) {
     // ignore errors
+    cli.error(error)
   }
 }
